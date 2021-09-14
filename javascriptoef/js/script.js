2bc7099d1d0918f65a246c5cc5488e9a298bb8c3
Michiel Verschaeve <michielverschaeverd@gmail.com>
2021-09-14 15:01:47 +0200

    eerste commit

---
+++ javascriptoef/js/script.js
@@ -0,0 +1,48 @@
+/*
+var naam = prompt("Geef uw naam in");
+var leeftijd = parseInt(prompt("Geef uw leeftijd in"));
+
+console.log(naam);
+console.log(leeftijd);
+
+if(leeftijd > 18) {
+console.log(naam + ", u mag deelnemen aan de trekking van de loterij");
+}else if (leeftijd >= 16) {
+console.log(naam + ", u mag mits toestemming van u ouders deelnemen aan de loterij");
+} else {
+    console.log(naam + ", u mag niet deelnemen aan de loterij");
+}*/
+
+/*var leeftijd = prompt("Wat is uw leeftijd?");
+var aantalVakjes = prompt("Hoeveel vakjes wil je kopen?")
+
+if (leeftijd >= 18) {
+    if (aantalVakjes == 12) {
+        console.log("De prijs is 20 euro");
+    } else if (aantalVakjes == 2) {
+        console.log("De prijs is 4 euro");
+    } else if (aantalVakjes == 4) {
+        console.log("De prijs is 8 euro");
+    } else if (aantalVakjes == 6) {
+        console.log("De prijs is 12 euro");
+    } else if (aantalVakjes == 8) {
+        console.log("De prijs is 16 euro");
+    } else if (aantalVakjes == 10)
+        console.log("De prijs is 18 euro");
+} else {
+    console.log(alert("U mag niet meedoen aan het spel"));
+}*/
+
+var geboorteJaar = parseInt(prompt("Geef uw geboortejaar in"));
+var lopendJaar = parseInt(prompt("Geef het lopen jaar in"));
+
+if ((lopendJaar - geboorteJaar) >= 18) {
+    console.log("Vanaf nu mag, kan en beslis ik alles, binnen de wettelijke grenzen.");
+} else {
+    console.log("Gelukkig heb ik mijn ouders die alles voor me regelen");
+}
+
+yeeet
+
+
+
