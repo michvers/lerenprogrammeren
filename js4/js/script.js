312eb38b8d72a0886b9257558be7e8ee68c7f8af
Michiel Verschaeve <michielverschaeverd@gmail.com>
2021-09-15 09:32:18 +0200

    meervoudige selectie

---
+++ js4/js/script.js
@@ -0,0 +1,24 @@
+var onderwijs = prompt("Geef uw onderwijsintelling");
+switch(onderwijs){
+    case 'vdab':
+        console.log('cursus is gegeven door VDAB');
+        break;
+    case 'syntra':
+        console.log('cursus is gegeven door Syntra');
+        break;
+    default:
+        console.log('gegeven door andere instelling');
+}
+
+/*
+//enkelvoudige selectie shorthand
+var x = 1;
+if(x == 1){
+    //code
+}else{
+    //code
+}
+
+x==1 ? //code : //code*/
+
+
