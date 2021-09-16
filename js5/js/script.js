7cf671bf2ab7e0f9400143876df190b6ee3cc7c8
Michiel Verschaeve <michielverschaeverd@gmail.com>
2021-09-16 16:00:54 +0200

    tem oef 8

--- js5/js/script.js
+++ js5/js/script.js
@@ -49,6 +49,7 @@ console.log("De totale som van" + aantalGetallen + "getallen is " + resultaat);
 
 */
 
+/*
 var teller = 1;
 var aantalGetallen= parseInt(prompt("Geef het aantal sterren in"));
 var ster = "*";
@@ -58,22 +59,60 @@ for(teller;teller<=aantalGetallen;teller++){
     console.log(resultaat);
 }
 
-
-console.log(resultaat.substr(0,4));
-
-for(teller; teller>=aantalGetallen;teller--){
-    resultaat = resultaat - ster;
-    console.log(resultaat);
+for(teller;teller>0;teller--){
+    console.log(resultaat.substr(0, teller))
 }
 
+*/
+
+/*
+var getal = parseInt(prompt("Geef een getal in"));
+var fac = 1;
+for(var i=1;i <= getal;i++){
+    fac *=i;
+}
+console.log(fac);
+*/
+// oefening 5
+/*var einde = "99";
+var resultaat = "";
+for(var i=0;i<=einde;i++){
+
+    if(i<10){
+        resultaat = resultaat + "0" + i + ",";
+    }else{
+        resultaat = resultaat + i + ",";
+    }
+}
+
+console.log(resultaat.substring(0,resultaat.length-1));*/
 
 
+/*
+//oefening 6
+var tekenreeks = "tekenreeks"
+var i = ""
+console.log(tekenreeks.length);
+
+*/
+
+//oefening 7
+/*
+var resultaat = "";
+var getal = prompt("Geef een getal in");
+console.log(getal);
+for(var i=0;i<=getal;i++){
+    if(i%2==0){
+        resultaat = resultaat + i;
+    }else{
+    }
+}
+console.log(resultaat);
+*/
 
 
+//oefening 8
 
-
-
-
-
-
+var getal = prompt("Geef een getal in");
+for(var i=1;)
 
